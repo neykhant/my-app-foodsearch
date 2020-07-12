5fdb80c0142a5f428555a6c7831f5ad01f69b7f3
khantmawhein <kokhant228abc@gmail.com>
2020-07-13 01:12:07 +0630

    Updates

---
+++ precache-manifest.8f58f2a7ae8f102f9c5996b81fd04864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2dfaebb47d95ac36603d80a681c58ad",
+    "url": "/my-app-foodsearch/index.html"
+  },
+  {
+    "revision": "95cdcfe3d4d507e32a1b",
+    "url": "/my-app-foodsearch/static/css/main.c46bc189.chunk.css"
+  },
+  {
+    "revision": "4f54ba01e06a1169b2ed",
+    "url": "/my-app-foodsearch/static/js/2.f1cc2272.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app-foodsearch/static/js/2.f1cc2272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95cdcfe3d4d507e32a1b",
+    "url": "/my-app-foodsearch/static/js/main.31a28115.chunk.js"
+  },
+  {
+    "revision": "df6d50959969477497a3",
+    "url": "/my-app-foodsearch/static/js/runtime-main.fb3ff200.js"
+  }
+]);
